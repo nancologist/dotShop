101f5b250ab2dd1aec2f02259b522105612de713
Heisenberg <nancologist@gmail.com>
2021-02-13 10:09:24 +0100

    Prevent spamming ADD button.

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,6 +1,7 @@
-
+const { DB } = require('../DB')
 const BaseController = require('./index');
 const Product = require('../models/product');
+const { apple, banana, orange } = require('../assets/images/urls');
 
 class AdminController extends BaseController {
     constructor(mainRoute) {
@@ -9,10 +10,16 @@ class AdminController extends BaseController {
     }
 
     addSomeProducts (req, res, next) {
-        new Product('13j4k', 'Apple', 0.65).save();
-        new Product('jkl42', 'Orange', 0.8).save();
-        new Product('mv2kl', 'Banana', 1.3).save();
-    
+        const areProdsAlreadyStored = DB.products.length >= 3;
+        if (areProdsAlreadyStored) {
+            res.json({ msg: 'The products are already stored in database!' });
+            return;
+        }
+
+        new Product('13j4k', 'Apple', 0.65, apple).save();
+        new Product('mv2kl', 'Banana', 1.3, banana).save();
+        new Product('jkl42', 'Orange', 0.8, orange).save();
+
         res.json({ msg: 'Products successfully added!' });
     };
 

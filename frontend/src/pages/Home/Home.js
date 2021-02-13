bb13d97f46bc9601fc0543d45772faa4a3cdf883
Heisenberg <nancologist@gmail.com>
2021-02-13 21:09:53 +0100

    Add home page.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,11 +1,14 @@
-import { Fragment } from 'react'
+import './Home.css';
 
 const Home = () => {
     return (
-        <Fragment>
-            <h1>Home Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odit qui atque labore quasi ipsum tempora, dolorum, molestiae minima cum impedit illum et sequi voluptas velit hic deserunt, consectetur modi? Eum, maxime cupiditate? Non placeat libero voluptas cumque voluptatum blanditiis nobis, nam mollitia natus. Assumenda, eius incidunt! Nihil animi facere possimus ex amet, molestias, placeat laudantium, esse id quas nam corporis! Nesciunt placeat porro, architecto repellat mollitia, aliquam sed incidunt ad quis molestiae eius magni, eum fugit aspernatur repellendus omnis obcaecati? Quos minima tenetur consectetur nihil odit ipsum, beatae reiciendis odio vel dolorem, officiis assumenda iste optio perferendis. Aut molestiae similique eum nesciunt ipsum, pariatur ab iste sunt? Non itaque veniam quam aut minus ducimus eum quas, nobis quia ab provident ipsam eaque id qui esse eius corporis architecto rem, repellendus possimus odio omnis ut. Aliquid, nihil consequuntur doloribus dolorum veritatis, autem ipsa sunt, et quos aliquam dolore odit? Harum omnis possimus nobis ad hic quae rem in, incidunt, quidem recusandae dolorem repudiandae cupiditate voluptatibus nisi sed veritatis suscipit explicabo neque minus quos, beatae reprehenderit exercitationem? Quibusdam nisi suscipit similique possimus sit rerum vitae odio, laborum ex ab eveniet quos minus repellat tempora voluptatem cum modi, ipsum explicabo exercitationem. Porro, quasi ad animi expedita rerum iste praesentium dolorem tenetur at repellat aut suscipit molestias obcaecati, architecto aliquid quis aperiam rem. Modi doloribus, qui laudantium assumenda autem nulla minus quos quam tempore repudiandae, dolorum cum! Repudiandae vitae dolorum a quis autem commodi recusandae delectus voluptatum quo laboriosam nostrum minus deserunt veniam in culpa, tempore mollitia non eos? Distinctio quos, atque pariatur iste ex id aliquid beatae inventore officia similique natus maiores nobis temporibus, sequi impedit incidunt. Omnis exercitationem ipsa, atque aut pariatur voluptatibus! Id rerum reiciendis minima officia dolorum enim molestiae sit mollitia voluptatum debitis commodi omnis quam ex doloremque soluta rem culpa eum libero, earum sunt saepe delectus quaerat, et ullam. Rerum iure eum non sunt error officia excepturi cupiditate dolorem illum voluptatibus laboriosam provident, facilis</p>
-        </Fragment>
+        <div className="home">
+            <div className="home__text">
+                <h1>Welcome to our online shop!</h1>
+                <p>Here we offer fresh organic vegetables and fruits form the region.</p>
+                <p>Enjoy shopping!</p>
+            </div>
+        </div>
     )
 };
 

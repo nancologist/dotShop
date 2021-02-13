f80a342390ed0ca1709745a95b0679b912886ce1
Heisenberg <nancologist@gmail.com>
2021-02-13 11:12:47 +0100

    Handle response message actions.

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,12 +1,20 @@
 export const actionTypes = {
-    GET_PRODUCTS: 'GET_PRODUCTS'
+    GET_PRODUCTS: 'GET_PRODUCTS',
+    POST_PRODUCTS: 'POST_PRODUCTS',
 }
 
-const { GET_PRODUCTS } = actionTypes;
+const { GET_PRODUCTS, POST_PRODUCTS } = actionTypes;
 
 export const getProducts = (products) => {
     return {
         type: GET_PRODUCTS,
         products
     }
-}
+};
+
+export const postProducts = (msg) => {
+    return {
+        type: POST_PRODUCTS,
+        msg
+    }
+};

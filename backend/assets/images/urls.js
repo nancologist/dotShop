a420b48efca925c11ed9f054ee54e19675a3f927
Heisenberg <nancologist@gmail.com>
2021-02-13 11:08:48 +0100

    Add hosting urls.

---
+++ backend/assets/images/urls.js
@@ -0,0 +1,7 @@
+HOSTING_URL = 'https://i.ibb.co/';
+
+// module.exports.bananaL = HOSTING_URL + 'jrB0n5D/banana-l.png';
+module.exports.apple = HOSTING_URL + 'K0yJsgG/apple-s.png';
+module.exports.banana = HOSTING_URL + 'xSb7q75/banana-s.png';
+module.exports.orange = HOSTING_URL + 'j6zQfHv/orange-s.png';
+

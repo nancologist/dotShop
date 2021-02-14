1eb0f2f72714f78276170a7ad196fcabc9bc4e2a
Heisenberg <nancologist@gmail.com>
2021-02-14 20:08:18 +0100

    Add msg for adding item to the cart.

--- backend/app.js
+++ backend/app.js
@@ -16,4 +16,3 @@ new Server({
         allowCORS
     ]
 });
- 
--- frontend/src/pages/Shop/Shop.js
+++ frontend/src/pages/Shop/Shop.js
@@ -9,16 +9,24 @@ import './Shop.css';
 const Shop = (props) => {
     const { getProducts } = props;
     useEffect(() => { getProducts() }, [getProducts]);
+    const [msgClass, setMsgClass] = useState(['shop__msg'])
 
     const handleClick = (id) => {
         props.addToCart(id)
+
+        const isMsgInvisible = msgClass.length < 2;
+        if (isMsgInvisible) {
+            setMsgClass(['shop__msg', 'shop__msg--show']);
+            setTimeout(() => { setMsgClass(['shop__msg']) }, 2000);
+        }
+
     }
 
     const hasArticles = props.products.length > 0;
     return (
         <div className="shop">
             {hasArticles ?
-                <div>{
+                <div className="shop__list">{
                     props.products.map(prod => (
                         <Card
                             key={prod.id} product={prod}
@@ -26,8 +34,11 @@ const Shop = (props) => {
                         />
                     ))
                 }</div>
-                : <div className="msg-box--error">Currently there's no product available, please come back later again.</div>
+                : <div className="shop__list--empty">Currently there's no product available, please come back later again.</div>
             }
+            <div className={msgClass.join(' ')}>
+                <span>Item added to cart!</span>
+            </div>
         </div>
     );
 };

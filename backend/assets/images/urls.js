2fa46b43732980fc8fae8de00747e3694843f376
Heisenberg <nancologist@gmail.com>
2021-02-16 13:40:44 +0100

    Add more pics.

--- backend/assets/images/urls.js
+++ backend/assets/images/urls.js
@@ -4,4 +4,15 @@ HOSTING_URL = 'https://i.ibb.co/';
 module.exports.apple = HOSTING_URL + 'K0yJsgG/apple-s.png';
 module.exports.banana = HOSTING_URL + '8cccfyy/banana-s.png';
 module.exports.orange = HOSTING_URL + 'j6zQfHv/orange-s.png';
-
+module.exports.avocado = HOSTING_URL + 'YdtSLxW/avocado-s.png';
+module.exports.blueberry = HOSTING_URL + 'SNG8RTy/blueberry-s.png';
+module.exports.carrot = HOSTING_URL + 'yPZ6xQs/carrot-s.png';
+module.exports.celeriac = HOSTING_URL + 'rdqFMbH/celeriac-s.png';
+module.exports.cucamber = HOSTING_URL + 'zNtkfMG/cucamber-s.png';
+module.exports.eggplant = HOSTING_URL + '17km5Q6/eggplant-s.png';
+module.exports.ginger = HOSTING_URL + 'wgtMqNb/ginger-s.png';
+module.exports.kohlrabi = HOSTING_URL + 'm9KsddL/kohlrabi-s.png';
+module.exports.leeks = HOSTING_URL + 'g7Dr14K/leeks-s.png';
+module.exports.peach = HOSTING_URL + '61HDwPX/peach-s.png';
+module.exports.potato = HOSTING_URL + '3y4727f/potato-s.png';
+module.exports.tomato = HOSTING_URL + 'pzDGRDp/tomato-s.png';

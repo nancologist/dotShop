b8037de9ac1968c2fe9c5053f40ea8a18df7d4af
Heisenberg <nancologist@gmail.com>
2021-02-16 12:25:29 +0100

    Add some explanations about admin cmp.

--- frontend/src/pages/Admin/Admin.js
+++ frontend/src/pages/Admin/Admin.js
@@ -20,8 +20,13 @@ const Admin = () => {
         <div className="admin">
             <h1>Admin Page</h1>
             <p>Here we would have CRUD functionalities for Admin to control Products, but it's not a main goal of project. So for now we mock the CREATE functionality to add some products to our shop.</p>
+            <hr/>
             <button className="admin__add-btn" onClick={postAddProducts}>ADD PRODUCTS & COUPONS TO DB</button>
             <SlidingMsg ref={msgEl} time={1500} />
+            <div>
+                <h2>Orders:</h2>
+                Here would have admin an overview of the submitted Orders by customer, which are fetched from Backend > Database
+            </div>
         </div>
     )
 };

b5df6068ce59c171527c64614ae5b5acc7e6018a
Heisenberg <nancologist@gmail.com>
2021-02-28 16:54:32 +0100

    Ready to Deploy.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const Server = require('./Server/server');
 const ShopController = require('./controllers/shop');
 const AdminController = require('./controllers/admin');
 const { allowCORS } = require('./middlewares');
 
+const PORT = process.env.PORT || 8080;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000'
+
 new Server({
-    port: 8989,
+    port: PORT,
     controllers: [
         new AdminController('/admin'),
         new ShopController('/shop'),
     ],
     middlewares: [
         bodyParser.json(),
-        allowCORS
+        allowCORS(CLIENT_URL),
+        helmet()
     ]
 });
--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,6 +1,8 @@
-module.exports.allowCORS = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+module.exports.allowCORS = (url) => {
+    return (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', url);
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    }
 }
